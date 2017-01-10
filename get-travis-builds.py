a1bd7d58984c1d1725349fc4f2961b362cc99d54
Daniel Miller <dmiller@dimagi.com>
2017-01-10 11:55:26 -0500

    Improve downloader and add plotter

--- get-travis-builds.py
+++ get-travis-builds.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+from __future__ import print_function
 import argparse
 import csv
 import json
@@ -20,39 +21,90 @@ def main():
         help="Maximum number of requests to make. Each request pulls 25 builds.")
     parser.add_argument("--before",
         help="Get builds numbered before (less than) this build.")
-    #parser.add_argument("--format", choices=["csv", "json"],
-    #    help="Output format: csv or json")
+    parser.add_argument("--format", choices=["csv", "json"], default="json",
+        help="Output format: csv or json (default is json)")
+    parser.add_argument("--builds-json",
+        help="Previously downloaded JSON file to parse instead of Travis API")
 
     args = parser.parse_args()
 
-    print("[")
-    for build in iter_builds(args.slug, args.limit, args.before):
-        print(json.dumps(build) + ",")
-    print("]")
+    print_output = {"json": json_formatter, "csv": csv_formatter}[args.format]
+    try:
+        if args.builds_json:
+            with open(args.builds_json) as fh:
+                builds = json.load(fh)
+        else:
+            builds = iter_builds(args.slug, args.limit, args.before)
+        print_output(status_printer(builds))
+    except requests.exceptions.RequestException as err:
+        print_status("Bad API response: {}".format(err))
+    except KeyboardInterrupt:
+        print_status("aborting...")
+
+
+def print_status(message):
+    sys.stderr.write(message + "\n")
 
 
 def iter_builds(slug, limit, before):
     url = URL_TEMPLATE.format(slug=slug)
     while limit > 0:
         params = {} if not before else {"after_number": before}
-        try:
-            resp = requests.get(url, params=params, headers=HEADERS)
-            if not resp.status_code == requests.codes.ok:
-                resp.raise_for_status()
-        except requests.exceptions.RequestException as err:
-            sys.stderr.write("Bad API response: {}\n".format(err))
-            return
+        resp = requests.get(url, params=params, headers=HEADERS)
+        if resp.status_code != requests.codes.ok:
+            resp.raise_for_status()
         if not resp.json()["builds"]:
             return
         for build in resp.json()["builds"]:
             yield build
         before = build["number"]
-        if not before:
-            # should never happen
-            return
         limit -= 1
-        if limit % 10 == 0:
-            sys.stderr.write("got {} builds".format(limit))
+
+
+def status_printer(builds):
+    # this function knows that builds are downloaded in batches of 25
+    for i, build in enumerate(builds):
+        yield build
+        if (i + 1) % 250 == 0:
+            print_status("processed {} builds".format((i + 1) / 25))
+
+
+def json_formatter(rows):
+    print("[")
+    first = True
+    try:
+        for row in rows:
+            if not first:
+                print(",")
+            print(json.dumps(row), end="")
+            first = False
+    finally:
+        print("\n]")
+
+
+def csv_formatter(rows):
+    first_cols = {header: i for i, header in enumerate([
+        "number",
+        "started_at",
+        "finished_at",
+        "duration",
+        "state",
+        "event_type",
+        "pull_request_number",
+    ])}
+    def header_key(header, nh=len(first_cols)):
+        return (first_cols.get(header, nh), header)
+    def stringify(value):
+        if isinstance(value, (dict, list)):
+            return json.dumps(value)
+        return unicode(value).encode("utf-8")
+    row = next(rows)
+    headers = sorted(row, key=header_key)
+    writer = csv.DictWriter(sys.stdout, headers)
+    writer.writerow({h: h for h in headers})
+    writer.writerow({key: stringify(value) for key, value in row.items()})
+    for row in rows:
+        writer.writerow({key: stringify(value) for key, value in row.items()})
 
 
 if __name__ == "__main__":
---
+++ plotter.py
@@ -0,0 +1,145 @@
+#! /usr/bin/env python
+import argparse
+import json
+import re
+from datetime import datetime
+
+import plotly
+from plotly.graph_objs import Scattergl, Layout, Figure
+
+ANNOTATE = set([
+    11380,
+    11573,
+    11901,
+])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path",
+        help="Path to file containing JSON list of builds.")
+    parser.add_argument("--max-diff", type=float, default=10.0,
+        help="Highlight builds more than this number of minutes above the "
+             "average of the last 50 builds.")
+    parser.add_argument("--wall-clock",
+        action="store_false", dest="duration", default=True,
+        help="If enabled, plot elapsed (wall clock) time instead "
+             "of total run time (duration) of test run.")
+#    parser.add_argument("--limit", type=int, default=40,
+#        help="Maximum number of requests to make. Each request pulls 25 builds.")
+
+    args = parser.parse_args()
+
+    with open(args.path) as fh:
+        builds = json.load(fh)
+
+    def keep(build):
+        return (
+            build["started_at"]
+            and build["state"] == "passed"
+            and build["pull_request_number"]
+#            and build["number"] > "36480"
+        )
+
+    def label(build):
+        return u'{pr} - {desc}'.format(
+            pr=build["pull_request_number"],
+            desc=build["pull_request_title"].replace('<', ''),
+        )
+
+    max_diff = args.max_diff
+    def is_near_average(rec_time, recents):
+        avg = sum([float(r["y"]) for r in recents]) / len(recents)
+        return (rec_time - avg) < max_diff
+
+    builds = [b for b in builds if keep(b)]
+    builds.sort(key=lambda b: getdate(b["started_at"]))
+    if args.duration:
+        y_title = 'Total Test Run Time (minutes)'
+        timefunc = lambda b: secs_to_mins(b["duration"])
+    else:
+        y_title = 'Elapsed Wall Clock Time (minutes)'
+        timefunc = elapsed_minutes
+
+    normals = []
+    abnormals = []
+    annotations = []
+    recents = []
+    for build in builds:
+        rec = {
+            "x": getdate(build["started_at"]),
+            "y": timefunc(build),
+            "text": label(build),
+        }
+        if build["pull_request_number"] in ANNOTATE:
+            annotations.append(rec)
+        if len(recents) < 50:
+            recents.append(rec)
+            normals.append(rec)
+        else:
+            if is_near_average(rec["y"], recents):
+                normals.append(rec)
+            else:
+                abnormals.append(rec)
+            recents.pop(0)
+            recents.append(rec)
+
+    plotly.offline.plot({
+        "data": [
+            Scattergl(
+                mode='markers+text',
+                name='Average',
+                marker={"color": "rgba(0, 147, 0, 0.9)"},
+                **scatter_params(normals)
+            ),
+            Scattergl(
+                mode='markers+text',
+                name='+{} min'.format(max_diff),
+                marker={"color": "rgba(0, 128, 255, 0.9)"},
+                **scatter_params(abnormals)
+            ),
+            Scattergl(
+                mode='markers+text',
+                name='Event'.format(max_diff),
+                marker={"color": "rgba(200, 0, 0, 0.9)"},
+                **scatter_params(annotations)
+            ),
+        ],
+        "layout": Layout(
+            title="Test Build Times",
+            hovermode="closest",
+            xaxis=dict(
+                title='Build Date',
+                type='date',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            yaxis=dict(
+                title=y_title,
+                ticklen=5,
+                gridwidth=2,
+            ),
+        )
+    })
+
+
+def getdate(isodate):
+    return datetime.strptime(isodate, "%Y-%m-%dT%H:%M:%SZ")
+
+
+def secs_to_mins(secs):
+    return secs / 60
+
+def elapsed_minutes(build):
+    delta = getdate(build["finished_at"]) - getdate(build["started_at"])
+    return secs_to_mins(delta.total_seconds())
+
+def scatter_params(records):
+    return {
+        "x": [rec["x"] for rec in records],
+        "y": [rec["y"] for rec in records],
+        "text": [rec["text"] for rec in records],
+    }
+
+
+if __name__ == "__main__":
+    main()
